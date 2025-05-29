311d4a9cbf6b29ac1d091056bcc2b67f624dfcf2
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2025-05-28 17:11:02 -0700

    more macro progress

--- wayland_scanner/scanner_macro/src/lib.rs
+++ wayland_scanner/scanner_macro/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate proc_macro;
+use change_case::pascal_case;
 use proc_macro::TokenStream;
 use serde::Deserialize;
 use syn::{LitStr, parse_macro_input};
@@ -42,8 +43,8 @@ fn generate_code_for_protocol(path: &Path) {
     println!("{}\n", path.to_str().unwrap());
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
-    let p: RawProtocol = quick_xml::de::from_reader(reader).unwrap();
-    println!("{:#?}", p);
+    let p = quick_xml::de::from_reader::<BufReader<File>, RawProtocol>(reader).unwrap();
+    println!("{:#?}", Protocol::from(p));
 }
 
 #[derive(Debug, Deserialize)]
@@ -160,6 +161,7 @@ struct RawDescription {
     content: Option<String>,
 }
 
+#[derive(Debug)]
 struct Protocol {
     name: String,                     // snake_case
     copyright: Option<Copyright>, // leading and trailing whitespace removed, split at /n, with each line prefixed by //
@@ -167,8 +169,27 @@ struct Protocol {
     interfaces: Vec<Interface>,       // one or more
 }
 
+impl From<RawProtocol> for Protocol {
+    fn from(value: RawProtocol) -> Self {
+        Self {
+            name: value.name,
+            copyright: value.copyright.map(Copyright::from),
+            description: value.description.map(Description::from),
+            interfaces: value.interfaces.into_iter().map(Interface::from).collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Copyright(String);
 
+impl From<RawCopyright> for Copyright {
+    fn from(value: RawCopyright) -> Self {
+        Self(value.0.trim().to_string())
+    }
+}
+
+#[derive(Debug)]
 struct Interface {
     name: String,      // snake_case
     type_name: String, // PascalCase
@@ -179,11 +200,48 @@ struct Interface {
     enums: Vec<Enum>,
 }
 
+impl From<RawInterface> for Interface {
+    fn from(value: RawInterface) -> Self {
+        let type_name = pascal_case(&value.name);
+        let mut requests = Vec::<Request>::new();
+        let mut events = Vec::<Event>::new();
+        let mut enums = Vec::<Enum>::new();
+        for elem in value.elements {
+            match elem {
+                Element::Request(req) => requests.push(req.into()),
+                Element::Event(ev) => events.push(ev.into()),
+                Element::Enum(en) => enums.push(en.into()),
+            };
+        }
+        Self {
+            name: value.name,
+            type_name,
+            // max_version: value.version.parse().unwrap(),
+            max_version: 1,
+            description: value.description.map(Description::from),
+            requests,
+            events,
+            enums,
+        }
+    }
+}
+
+#[derive(Debug)]
 enum RequestType {
     Default,
     Destructor,
 }
 
+impl From<Option<String>> for RequestType {
+    fn from(value: Option<String>) -> Self {
+        match value.as_deref() {
+            Some("destructor") => Self::Destructor,
+            _ => Self::Default,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Request {
     name: String, // snake_case
     r#type: RequestType,
@@ -193,11 +251,37 @@ struct Request {
     args: Vec<Arg>,
 }
 
+impl From<RawRequest> for Request {
+    fn from(value: RawRequest) -> Self {
+        Self {
+            name: value.name,
+            r#type: value.r#type.into(),
+            // since: value.since.map_or(1, |s| s.parse().unwrap()),
+            since: 1,
+            // deprecated_since: value.deprecated_since.map(|s| s.parse().unwrap()),
+            deprecated_since: Some(0),
+            description: value.description.map(Description::from),
+            args: value.args.into_iter().map(Arg::from).collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
 enum EventType {
     Default,
     Destructor,
 }
 
+impl From<Option<String>> for EventType {
+    fn from(value: Option<String>) -> Self {
+        match value.as_deref() {
+            Some("destructor") => Self::Destructor,
+            _ => Self::Default,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Event {
     name: String, // snake_case
     r#type: EventType,
@@ -207,21 +291,45 @@ struct Event {
     args: Vec<Arg>,
 }
 
-enum EnumType {
-    Int,
-    UInt,
-    Bitfield,
+impl From<RawEvent> for Event {
+    fn from(value: RawEvent) -> Self {
+        Self {
+            name: value.name,
+            r#type: value.r#type.into(),
+            // since: value.since.map_or(1, |s| s.parse().unwrap()),
+            since: 1,
+            // deprecated_since: value.deprecated_since.map(|s| s.parse().unwrap()),
+            deprecated_since: Some(0),
+            description: value.description.map(Description::from),
+            args: value.args.into_iter().map(Arg::from).collect(),
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Enum {
     name: String,      // snake_case
     type_name: String, // PascalCase
     since: u32,
-    r#type: EnumType,
     description: Option<Description>,
     entries: Vec<Entry>,
 }
 
+impl From<RawEnum> for Enum {
+    fn from(value: RawEnum) -> Self {
+        let type_name = change_case::pascal_case(&value.name);
+        Self {
+            name: value.name,
+            type_name,
+            // since: value.since.map_or(1, |s| s.parse().unwrap()),
+            since: 1,
+            description: value.description.map(Description::from),
+            entries: value.entries.into_iter().map(Entry::from).collect(),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Entry {
     name: String,       // snake_case
     valid_name: String, // PascalCase, invalid identifiers fixed
@@ -233,9 +341,27 @@ struct Entry {
 }
 
 impl From<RawEntry> for Entry {
-    fn from(value: RawEntry) -> Self {}
+    fn from(value: RawEntry) -> Self {
+        let is_valid_ident = syn::parse_str::<syn::Ident>(value.name.as_str()).is_ok();
+        println!("{} is valid identifier: {}", value.name, is_valid_ident);
+        // let numeric_value = value.name.parse().unwrap();
+        let numeric_value = 0u32;
+        let valid_name = change_case::pascal_case(&value.name);
+        Self {
+            name: value.name,
+            valid_name,
+            value: numeric_value,
+            summary: value.summary,
+            // since: value.since.map_or(1, |s| s.parse().unwrap()),
+            since: 1,
+            // deprecated_since: value.deprecated_since.map(|s| s.parse().unwrap()),
+            deprecated_since: Some(0),
+            description: value.description.map(Description::from),
+        }
+    }
 }
 
+#[derive(Debug)]
 enum ArgType {
     Int,
     UInt,
@@ -269,6 +395,7 @@ impl From<(String, Option<String>, Option<String>)> for ArgType {
     }
 }
 
+#[derive(Debug)]
 struct Arg {
     name: String,
     r#type: ArgType,
@@ -292,6 +419,7 @@ impl From<RawArg> for Arg {
     }
 }
 
+#[derive(Debug)]
 struct Description {
     summary: String,
     content: Option<String>,
@@ -299,15 +427,9 @@ struct Description {
 
 impl From<RawDescription> for Description {
     fn from(value: RawDescription) -> Self {
-        let content = if let Some(content) = value.content {
-            Some(content.trim().to_string())
-        } else {
-            None
-        };
-
         Self {
             summary: value.summary,
-            content,
+            content: value.content.map(|s| s.trim().to_string()),
         }
     }
 }
--- wayland_scanner/src/lib.rs
+++ wayland_scanner/src/lib.rs
@@ -3,6 +3,5 @@ mod tests {
     #[test]
     fn does_it_work() {
         scanner_macro::generate_client_protocols!("/usr/share/wayland/");
-        scanner_macro::generate_client_protocols!("/usr/share/wayland-protocols/");
     }
 }
